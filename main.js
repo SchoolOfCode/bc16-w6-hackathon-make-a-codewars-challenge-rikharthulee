151804e341275daf682f0b802f5a6fed2ff8548e
rikharthulee <rikharthulee@gmail.com>
2024-03-15 11:53:08 +0000

    Removed solutions from main.js

--- main.js
+++ main.js
@@ -3,21 +3,7 @@
  *
  * Assuming all US$100 bills, an average-sized briefcase (25" x 18" x 4") can fit US$2,400,000.
  *
- * How many briefcases do we need!
+ * How many briefcases do we need! - The result must be in whole briefcases
  */
 
-// example of function used to pass the test
-export function howManyCases(number) {
-  // create a variable for how many $100 dollar bills fit in a case
-  const fullCase = 2400000;
-  // number divided by 2400000
-  return number / fullCase;
-}
-
-export function howManyWholeCases(number) {
-  // create a variable for how many $100 dollar bills fit in a case
-  const fullCase = 2400000;
-  // number divided by fullCase
-  const answer = number / fullCase;
-  return Math.ceil(answer);
-}
+export function howManyWholeCases(number) {}
